c184f46b49dcb9f1c5a92032066f3fbccb217e87
Kacper Pietkun <71560847+Kacper-Pietkun@users.noreply.github.com>
2020-09-26 16:18:21 +0200

    Rotation of enemies
    left or right side

--- BrainDefender/Assets/Scripts/Enemy/EnemySpawner.cs
+++ BrainDefender/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
     public void SpawnEnemy(float movementSpeed, int healthPoints, int attackDamage, int weight, float minimalDistance)
     {
         GameObject enemy = Instantiate(EnemyPrefab, GetRandomPosition(), EnemyPrefab.transform.rotation);
+        RotateEnemy(enemy);
         EnemyStatistics enemyStatistics = enemy.GetComponent<EnemyStatistics>();
         enemyStatistics.MovementSpeed = movementSpeed;
         enemyStatistics.HealtPoints = healthPoints;
@@ -18,10 +19,12 @@ public class EnemySpawner : MonoBehaviour
 
 
     private int maxPosition;
+    private GameObject brain;
 
     private void Awake()
     {
         Random.InitState((int)System.DateTime.Now.Ticks);
+        brain = GameObject.FindGameObjectWithTag(Tags.Brain);
         maxPosition = 13;
     }
 
@@ -41,5 +44,14 @@ public class EnemySpawner : MonoBehaviour
             posY = Random.Range(0, 2) == 0 ? maxPosition : -maxPosition;
         }
         return new Vector3(posX, posY, 0);
+    }
+
+    // We have to check if an enemy is going to the brain from the left or the right side and set its rotation properly
+    // Default rotation fits the situation when the enemy comes from the left side
+    private void RotateEnemy(GameObject enemy)
+    {
+        Vector3 result = enemy.transform.position - brain.transform.position;
+        if (result.x > 0)
+            enemy.transform.rotation = new Quaternion(transform.rotation.x, 180, transform.rotation.y, 0);
     }
 }
